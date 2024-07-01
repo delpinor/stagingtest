7a92893cb13691e0ed7557c3a46855f4a5928cde
delpinor <delpinor@gmail.com>
2024-06-30 22:40:17 -0300

    aa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SaludoComponent } from './saludo/saludo.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent }
+  { path: "", component: HomeComponent },
+  { path: "/home", component: HomeComponent },
+  { path: "/saludos", component: SaludoComponent },
 ];
 
 @NgModule({
